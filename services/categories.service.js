c3777319cf8b550c6ba3d72b1063fb1397c1c93b
fahadhossain24 <fahadhossaim24@gmail.com>
2023-11-08 00:00:05 +0600

    update

--- services/categories.service.js
+++ services/categories.service.js
@@ -2,10 +2,6 @@ const { getCategories } = require("../middleware/utils/database");
 const db = require("../server");
 
 exports.getCategoriesService = async () => {
-    try {
-        const data = await getCategories([]);
-        return data;
-    } catch (error) {
-        return error.message
-    }
+    const result = await getCategories([]);
+    return result;
 }
