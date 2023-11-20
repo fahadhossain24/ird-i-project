bee119c65dc368cd210ad7846ef892a78346791d
Fahad Hossain <105900280+fahadhossain24@users.noreply.github.com>
2023-11-20 18:44:41 +0600

    Update errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -14,6 +14,7 @@ const productionErrorResponse = (error, res) => {
     })
 }
 
+
 exports.errorHandler = (error, req, res, next) => {
     error.statusCode = error.statusCode || 500;
     error.status = error.status || 'server error';
